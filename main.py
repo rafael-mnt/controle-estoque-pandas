7e4b7dceb328f4c2c40bf0a5568e51bdb6bd77bd
Rafael Monteiro <rafaelmnt@outlook.com.br>
2026-04-03 19:50:31 -0300

    Refactor main.py for improved menu functionality

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import pandas as pd
-from functions import *
-
-produtos = {
+from functions import cadastrar_produto, excluir_produto, registrar_entrada, registrar_saida
+df_produtos = pd.DataFrame({
     'ID': ['N1','N4','M3','C2'],
     'PRODUTO': ['Notebook Asus','Notebook Positivo', 'Celular Xiaomi', 'Carregador Samsung'],
     'PREÇO': [3500.00, 1500.00, 1950.00, 35.00],
     'QUANTIDADE': [32, 55, 26, 14]
-}
-
-df_produtos = pd.DataFrame(produtos)
+})
     
 while True:
-    print(f'\n{df_produtos}\n')
-
-    print('Menu Inicial:')
+    
+    print('\nMenu Inicial:')
     print('1 - Cadastrar Produto')
     print('2 - Excluir Produto')
     print('3 - Registrar Entrada')
@@ -29,7 +25,13 @@ while True:
         df_produtos = cadastrar_produto(df_produtos)
     elif opcao == '2':
         df_produtos = excluir_produto(df_produtos)
+    elif opcao == '3':
+        df_produtos = registrar_entrada(df_produtos)
+    elif opcao == '4':
+        df_produtos = registrar_saida(df_produtos)
+    elif opcao == '5':
+        print(f'\n{df_produtos}\n')
     else:
-        print('Vamo de novo')
+        print('Entrada inválida, utilize números de 0 a 5!')
         
 print('Programa Finalizado')
