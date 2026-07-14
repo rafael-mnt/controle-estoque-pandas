699a9e8ff802956916556cf983a6f13799b29e9a
Rafael Monteiro <rafaelmnt@outlook.com.br>
2026-07-14 20:04:03 -0300

    Adiciona script de criação das tabelas PostgreSQL

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE categoria (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE fornecedor (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	cpf_cnpj VARCHAR(18) UNIQUE NOT NULL,
+	telefone VARCHAR(20),
+	email VARCHAR(100)
+);
+
+CREATE TABLE produto (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	categoria_id INTEGER NOT NULL,
+	fornecedor_id INTEGER NOT NULL,
+	preco NUMERIC(10,2) NOT NULL,
+	estoque_atual INTEGER NOT NULL DEFAULT 0 CHECK (estoque_atual >= 0),
+	estoque_minimo INTEGER NOT NULL DEFAULT 0 CHECK (estoque_minimo >= 0),
+	data_de_cadastro DATE NOT NULL,
+
+	FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+	FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id)
+);
+
+CREATE TABLE movimentacao (
+	id SERIAL PRIMARY KEY,
+	produto_id INTEGER NOT NULL,
+	tipo VARCHAR(10) NOT NULL CHECK (tipo in ('Entrada', 'Saída')),
+	quantidade INTEGER NOT NULL,
+	data_movimentacao DATE NOT NULL,
+	observacao TEXT,
+
+	FOREIGN KEY (produto_id) REFERENCES produto(id)
+);
